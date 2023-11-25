7acb4c18cf4d4abb7af0aaf55520ebeeb0909571
Alejandro Diaz <72721553+DiaA6383@users.noreply.github.com>
2023-11-25 14:26:45 -0500

    bad scipt

--- scripts/clean.py
+++ scripts/clean.py
@@ -1,29 +1,41 @@
 import pandas as pd
 
-# Replace 'your_raw_data.csv' with the path to your raw data CSV file
-raw_data_path = 'data/raw/ACSST5Y2021.S1903_2023-11-14T204901/ACSST5Y2021.S1903-Data.csv'
+# Replace with the path to your CSV file
+csv_file_path = '../data/raw/ACSST5Y2021.S1903_2023-11-14T204901/ACSST5Y2021.S1903-Data.csv'
 
 # Replace these with the actual column names you want to keep
-columns_to_keep = ['ZIP_Code_Column_Name', 'Number_of_Households_Column_Name', 'Median_Income_Column_Name']
+columns_to_keep = [
+    'Geography',  # This is likely your ZIP code column
+    'Geographic Area Name',  # This contains 'ZCTA5 ' followed by the actual ZIP code
+    # Add your median income column names here
+]
 
 def clean_data(file_path, columns):
-    # Load the data
+    # Load the data with the specified columns only
     data = pd.read_csv(file_path, usecols=columns)
 
     # Check for missing values and decide how to handle them
-    # For example, you might fill missing values with the median or mode, or drop them entirely
     data = data.dropna()
 
-    # Ensure correct data types, e.g., ZIP Code might be better as a string rather than a numeric type
-    data['ZIP_Code_Column_Name'] = data['ZIP_Code_Column_Name'].astype(str)
+    # Ensure ZIP codes are read as strings to preserve leading zeros
+    data['Geography'] = data['Geography'].astype(str)
+
+    # Remove 'ZCTA5 ' from the 'Geographic Area Name' to keep only the ZIP code
+    data['Geographic Area Name'] = data['Geographic Area Name'].str.replace('ZCTA5 ', '')
 
     # Any additional cleaning steps would go here
-
+def clean_data(file_path, columns):
+    data = pd.read_csv(file_path, header=1, usecols=columns)
+    data = data.dropna()
+    data['Geography'] = data['Geography'].astype(str)
+    data['Geographic Area Name'] = data['Geographic Area Name'].str.replace('ZCTA5 ', '')
     return data
 
-# Use the function to clean the data
-cleaned_data = clean_data(raw_data_path, columns_to_keep)
+# Clean the data using the defined function
+cleaned_data = clean_data(csv_file_path, columns_to_keep)
 
-# Replace 'your_cleaned_data.csv' with the path to where you want to save your processed data CSV file
-cleaned_data_path = '/Users/alejandrodiaz/Documents/GitHub/DS_Project/data/processed'
+# Define the path to where you want to save your processed data CSV file
+cleaned_data_path = '/Users/alejandrodiaz/Documents/GitHub/DS_Project/data/processed/cleaned_data.csv'
+
+# Save the cleaned data to a CSV file, without the index
 cleaned_data.to_csv(cleaned_data_path, index=False)
