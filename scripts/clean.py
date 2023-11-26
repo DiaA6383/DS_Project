c38f7e5e31580b1679bddbe1c4a3ed583f35447b
Alejandro Diaz <72721553+DiaA6383@users.noreply.github.com>
2023-11-26 16:02:45 -0500

    formatting + cleanup

--- scripts/clean.py
+++ scripts/clean.py
@@ -5,10 +5,12 @@ CSV_FILE_PATH = '../data/raw/ACSST5Y2021.S1903_2023-11-14T204901/ACSST5Y2021.S19
 
 # Replace these with the actual column names you want to keep
 COLUMNS_TO_KEEP = [
-    'Geography',  # This is likely your ZIP code column
     'Geographic Area Name',  # This contains 'ZCTA5 ' followed by the actual ZIP code
     # Add your median income column names here
 ]
+
+# Define the path to where you want to save your processed data CSV file
+CLEANED_DATA_PATH = '/Users/alejandrodiaz/Documents/GitHub/DS_Project/data/processed/cleaned_data.csv'
 
 def clean_data(file_path, columns):
     """
@@ -16,15 +18,21 @@ def clean_data(file_path, columns):
     """
     data = pd.read_csv(file_path, header=1, usecols=columns)
     data = data.dropna()
-    data['Geography'] = data['Geography'].astype(str)
     data['Geographic Area Name'] = data['Geographic Area Name'].str.replace('ZCTA5 ', '')
+    #rename Geographic Area Name to Zip Code
+    data = data.rename(columns={'Geographic Area Name': 'Zip Code'})
     return data
 
-# Clean the data using the defined function
-cleaned_data = clean_data(CSV_FILE_PATH, COLUMNS_TO_KEEP)
 
-# Define the path to where you want to save your processed data CSV file
-CLEANED_DATA_PATH = '/Users/alejandrodiaz/Documents/GitHub/DS_Project/data/processed/cleaned_data.csv'
+def main():
+    """
+    This is the main function that calls the clean_data function.
+    """
+    cleaned_data = clean_data(CSV_FILE_PATH, COLUMNS_TO_KEEP)
 
-# Save the cleaned data to a CSV file, without the index
-cleaned_data.to_csv(CLEANED_DATA_PATH, index=False)
+    # Save the cleaned data to a CSV file, without the index, overwriting the old file
+    cleaned_data.to_csv(CLEANED_DATA_PATH, index=False)
+
+
+if __name__ == '__main__':
+    main()
