a88940dbd328c971a9282f372cf960944f0d3954
Alejandro Diaz <72721553+DiaA6383@users.noreply.github.com>
2023-11-27 00:18:28 -0500

    added comments

--- scripts/clean.py
+++ scripts/clean.py
@@ -2,20 +2,39 @@ import pandas as pd
 
 # Define the paths to the metadata, data, and cleaned data CSV files
 METADATA_FILE_PATH = '/Users/alejandrodiaz/Documents/GitHub/DS_Project/data/raw/ACSST5Y2021.S1903_2023-11-14T204901/metadata.csv'
-DATA_FILE_PATH = '/Users/alejandrodiaz/Documents/GitHub/DS_Project/data/raw/ACSST5Y2021.S1903_2023-11-14T204901/data.csv'
+CSV_FILE_PATH = '/Users/alejandrodiaz/Documents/GitHub/DS_Project/data/raw/ACSST5Y2021.S1903_2023-11-14T204901/data.csv'
 CLEANED_DATA_PATH = '/Users/alejandrodiaz/Documents/GitHub/DS_Project/data/processed/cleaned_data.csv'
 
-def load_csv(file_path):
-    return pd.read_csv(file_path, header=1)
 
-def get_rename_dict(metadata):
-    return dict(zip(metadata['Column Name'], metadata['Label']))
+def load_metadata(file_path):
+    """
+    Load metadata from a CSV file into a pandas DataFrame and create a dictionary for renaming columns.
+    """
+    metadata = pd.read_csv(file_path)
+    rename_dict = dict(zip(metadata['Column Name'], metadata['Label']))
+    return rename_dict
+
+
+def load_data(file_path):
+    """
+    Load data from a CSV file into a pandas DataFrame.
+    """
+    data = pd.read_csv(file_path, header=1)
+    return data
+
 
 def rename_columns(data, rename_dict):
+    """
+    Rename the columns of a DataFrame based on a renaming dictionary.
+    """
     data.rename(columns=rename_dict, inplace=True)
     return data
 
+
 def clean_data(data):
+    """
+    Clean the data by renaming columns, dropping unnecessary columns, and modifying column values.
+    """
     data.rename(columns={'Geographic Area Name': 'Zip Code'}, inplace=True)
     data['Zip Code'] = data['Zip Code'].str.replace('ZCTA5 ', '')
     data.drop(columns=['Geography'], inplace=True)
@@ -36,13 +55,17 @@ def clean_data(data):
     data.drop(data.columns[11::], axis=1, inplace=True)
     return data
 
+
 def main():
-    metadata = load_csv(METADATA_FILE_PATH)
-    rename_dict = get_rename_dict(metadata)
-    data = load_csv(DATA_FILE_PATH)
+    """
+    Main function to load, rename, clean, and save the data.
+    """
+    rename_dict = load_metadata(METADATA_FILE_PATH)
+    data = load_data(CSV_FILE_PATH)
     data = rename_columns(data, rename_dict)
     cleaned_data = clean_data(data)
     cleaned_data.to_csv(CLEANED_DATA_PATH, index=False)
+
 
 if __name__ == '__main__':
     main()
