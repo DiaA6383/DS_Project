97e2eda388672270b3077467582197c692f5e256
Alejandro Diaz <72721553+DiaA6383@users.noreply.github.com>
2023-11-26 22:15:36 -0500

    added some more cleanup

--- scripts/clean.py
+++ scripts/clean.py
@@ -1,56 +1,43 @@
 import pandas as pd
 
-# Paths to your CSV files
+# Define the paths to the metadata, data, and cleaned data CSV files
 METADATA_FILE_PATH = '/Users/alejandrodiaz/Documents/GitHub/DS_Project/data/raw/ACSST5Y2021.S1903_2023-11-14T204901/metadata.csv'
 CSV_FILE_PATH = '/Users/alejandrodiaz/Documents/GitHub/DS_Project/data/raw/ACSST5Y2021.S1903_2023-11-14T204901/data.csv'
-
-# Define the path to where you want to save your processed data CSV file
 CLEANED_DATA_PATH = '/Users/alejandrodiaz/Documents/GitHub/DS_Project/data/processed/cleaned_data.csv'
 
-def clean_column_names(column_names):
-    replacements = {
-        "Estimate!!": "",
-        "Margin of Error!!": "MOE_",
-        "Annotation of Estimate!!": "Note_",
-        "Annotation of Margin of Error!!": "Note_MOE_",
-        "Number!!": "",
-        "Percent Distribution!!": "PctDist_",
-        # Add more replacements as needed
-    }
-
-    clean_names = []
-    for name in column_names:
-        for to_replace, replacement in replacements.items():
-            name = name.replace(to_replace, replacement)
-        name = name.replace('!!', '_')
-        clean_names.append(name)
-
-    return clean_names
-
 def load_metadata(file_path):
+    # Load the metadata from the CSV file into a pandas DataFrame
     metadata = pd.read_csv(file_path)
+    # Create a dictionary that maps the 'Column Name' to the 'Label'
     rename_dict = dict(zip(metadata['Column Name'], metadata['Label']))
     return rename_dict
 
 def clean_data(file_path, rename_dict):
-    """
-    This function cleans the data from the CSV file.
-    """
+    # Load the data from the CSV file into a pandas DataFrame
     data = pd.read_csv(file_path, header=1)
-    data = data.rename(columns=rename_dict)  # Rename columns based on metadata
+    # Rename the columns based on the metadata
+    data = data.rename(columns=rename_dict)
+    # Clean the 'Geographic Area Name' column
     data['Geographic Area Name'] = data['Geographic Area Name'].str.replace('ZCTA5 ', '')
+    # Rename the 'Geographic Area Name' column to 'Zip Code'
     data = data.rename(columns={'Geographic Area Name': 'Zip Code'})
-    #drop Geography column
+    # Drop the 'Geography' column
     data = data.drop(columns=['Geography'])
+    # Drop columns that start with 'Annotation'
+    data = data.loc[:, ~data.columns.str.startswith('Annotation')]
+    # Drop columns that start with 'Margin of Error!!'
+    data = data.loc[:, ~data.columns.str.startswith('Margin of Error!!')]
+    # Replace 'Estimate!!' with '' in column names
+    data.columns = data.columns.str.replace('Estimate!!', '')
+    # Replace 'Number!!HOUSEHOLD INCOME BY RACE AND HISPANIC OR LATINO ORIGIN OF HOUSEHOLDER!!' with 'Number of ' in column names
+    data.columns = data.columns.str.replace('Number!!HOUSEHOLD INCOME BY RACE AND HISPANIC OR LATINO ORIGIN OF HOUSEHOLDER!!', 'Number of ')
     return data
 
 def main():
     # Load the renaming dictionary from the metadata file
     rename_dict = load_metadata(METADATA_FILE_PATH)
-    
     # Clean the data with the renaming dictionary
     cleaned_data = clean_data(CSV_FILE_PATH, rename_dict)
-
     # Save the cleaned data to a CSV file, without the index
     cleaned_data.to_csv(CLEANED_DATA_PATH, index=False)
 
