f21873ebbb4a70861bf852b65d138852ef4ebb41
Alejandro Diaz <72721553+DiaA6383@users.noreply.github.com>
2023-11-26 21:41:45 -0500

    added cleanup

--- scripts/column_peak.py
+++ scripts/column_peak.py
@@ -4,6 +4,7 @@ This script reads a CSV file and prints the second column.
 
 import pandas as pd
 
+
 def print_second_column_from_csv(file_path):
     """
     This function reads a CSV file and prints the second column.
@@ -19,6 +20,7 @@ def print_second_column_from_csv(file_path):
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
 
+
 def print_column_names(file_path):
     """
     print column names from cleaned data
@@ -32,12 +34,15 @@ def print_column_names(file_path):
         print(f"No data in file: {file_path}")
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
+
+
 def main():
     """
     This is the main function that calls the print_second_column_from_csv function.
     """
     cleaned_file_path = '/Users/alejandrodiaz/Documents/GitHub/DS_Project/data/processed/cleaned_data.csv'
     print_column_names(cleaned_file_path)
+
 
 if __name__ == '__main__':
     main()
