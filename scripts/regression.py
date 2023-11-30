cf684215795817e396f72051f591ae6f242e501b
Alejandro Diaz <72721553+DiaA6383@users.noreply.github.com>
2023-11-30 14:00:15 -0500

    Linear regression

--- scripts/regression.py
+++ scripts/regression.py
@@ -12,6 +12,26 @@ def load_data(data_path):
     data = pd.read_csv(data_path)
     return data
 
+def create_proxy_variable(data):
+    """
+    Create a proxy variable for purchase likelihood based on median income.
+    """
+    income_threshold = 80000
+    data['Purchase_Likelihood'] = (data['Median Income of all Families'] > income_threshold).astype(int)
+    return data
+
+def preprocess_data(data):
+    """
+    Preprocess the data by handling missing values.
+    """
+    # Option 1: Drop rows where 'Median Income of all Families' is NaN
+    # data = data.dropna(subset=['Median Income of all Families'])
+
+    # Option 2: Fill NaN values with the median or mean
+    data['Median Income of all Families'] = data['Median Income of all Families'].fillna(data['Median Income of all Families'].median())
+    
+    return data
+
 def train_model(X_train, y_train):
     """
     Train a linear regression model.
@@ -36,48 +56,31 @@ def plot_results(X_test, y_test, y_pred):
     plt.scatter(X_test, y_test, color='blue', label='Data Points')
     plt.plot(X_test, y_pred, color='red', label='Regression Line')
     plt.xlabel('Median Income of all Families')
-    plt.ylabel('Book Price')
-    plt.title('Linear Regression of Book Price vs Median Income')
+    plt.ylabel('Purchase Likelihood')
+    plt.title('Linear Regression of Purchase Likelihood vs Median Income')
     plt.legend()
     plt.show()
 
 def main():
-    # Define file paths
     current_dir = os.path.dirname(os.path.realpath(__file__))
     CLEANED_DATA_PATH = os.path.join(current_dir, '../data/processed/cleaned_data.csv')
 
-    # Load data
     data = load_data(CLEANED_DATA_PATH)
+    data = create_proxy_variable(data)
+    data = preprocess_data(data)
 
-    # Prepare the data
     X = data[['Median Income of all Families']]
-    y = data['Book_Price']
+    y = data['Purchase_Likelihood']
 
-    # Split the data
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Train the model
     model = train_model(X_train, y_train)
-
-    # Evaluate the model
     mse, r2, y_pred = evaluate_model(model, X_test, y_test)
 
-    # Output results
     print(f"Mean Squared Error: {mse}")
     print(f"R-squared: {r2}")
-    print("Coefficient (Slope):", model.coef_[0])
-    print("Intercept:", model.intercept_)
 
-    # Plot results
     plot_results(X_test, y_test, y_pred)
-
-    # Interpretation
-    if model.coef_[0] > 0:
-        print("There is a positive relationship between Median Income and Book Price.")
-    else:
-        print("There is a negative relationship between Median Income and Book Price.")
-
-    print("For every unit increase in Median Income, the Book Price increases by", model.coef_[0])
 
 if __name__ == '__main__':
     main()
