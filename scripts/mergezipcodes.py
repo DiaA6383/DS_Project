bde3de585593272f602bd6e6d8b9bab86efaeb90
Alejandro Diaz <72721553+DiaA6383@users.noreply.github.com>
2023-12-04 16:31:14 -0500

    documents

--- scripts/mergezipcodes.py
+++ scripts/mergezipcodes.py
@@ -4,43 +4,51 @@ import pandas as pd
 
 def merge_data_with_geocodes(data, gdf):
     """
-    Merge the main data DataFrame with the GeoDataFrame containing latitude and longitude.
-    
-    Parameters:
-    - data (pandas.DataFrame): The main data DataFrame.
-    - gdf (geopandas.GeoDataFrame): The GeoDataFrame containing latitude and longitude.
-    
+    Merge the given data DataFrame with geocodes based on the Zip Code.
+
+    Args:
+        data (pandas.DataFrame): The main data DataFrame.
+        gdf (geopandas.GeoDataFrame): The GeoDataFrame containing latitude and longitude.
+
     Returns:
-    - pandas.DataFrame: The merged DataFrame.
+        pandas.DataFrame: The merged DataFrame.
     """
     return data.merge(gdf[['ZCTA5CE10', 'latitude', 'longitude']], left_on='Zip Code', right_on='ZCTA5CE10')
 
-# Load the shapefile
+# Path to the shapefile containing geographic data
 shapefile_path = '/Users/alejandrodiaz/Documents/GitHub/DS_Project/data/raw/tl_2016_us_zcta510/tl_2016_us_zcta510.shp'
+
+# Read the shapefile into a GeoDataFrame
 gdf = gpd.read_file(shapefile_path)
 
-# Convert the geometries to a projected CRS before calculating the centroids
+# Convert the geometries to a projected CRS (Coordinate Reference System) before calculating the centroids
 gdf_projected = gdf.to_crs(epsg=2163)
 
 # Calculate centroids in the projected CRS
 gdf_projected['centroid'] = gdf_projected.geometry.centroid
 
-# Convert centroids back to geographic CRS to get latitude and longitude
+# Convert centroids back to geographic CRS to obtain latitude and longitude
 centroids_geo = gdf_projected['centroid'].to_crs(epsg=4326)
 gdf['latitude'] = centroids_geo.y
 gdf['longitude'] = centroids_geo.x
 
-# Load your existing data
+# Path to the existing data file
 data_path = '/Users/alejandrodiaz/Documents/GitHub/DS_Project/data/processed/cleaned_data.csv'
+
+# Read the existing data into a DataFrame
 data = pd.read_csv(data_path)
+
+# Convert Zip Code column to string type for consistency
 data['Zip Code'] = data['Zip Code'].astype(str)
 
 # Merge the dataframes on the Zip Code
 merged_data = merge_data_with_geocodes(data, gdf)
 
+# Check if 'ZCTA5CE10' column exists and drop it if it does
 if 'ZCTA5CE10' in merged_data.columns:
     merged_data = merged_data.drop(columns='ZCTA5CE10')
-# Save the merged data
+
+# Save the merged data to a CSV file
 merged_data.to_csv('/Users/alejandrodiaz/Documents/GitHub/DS_Project/data/processed/merged_data.csv', index=False)
 
 # Unit tests
@@ -62,16 +70,16 @@ class TestMergeDataWithGeocodes(unittest.TestCase):
         # Test the merge_data_with_geocodes function
         merged_data = merge_data_with_geocodes(self.data, self.gdf)
 
-        # Check for correct columns
+        # Check for correct columns in the merged data
         self.assertIn('Zip Code', merged_data.columns)
         self.assertIn('Other Data', merged_data.columns)
         self.assertIn('latitude', merged_data.columns)
         self.assertIn('longitude', merged_data.columns)
 
-        # Check for correct number of rows
+        # Check for the correct number of rows in the merged data
         self.assertEqual(len(merged_data), 3)
 
-        # Check for correct data
+        # Check for the correct data in the merged data
         self.assertEqual(merged_data.loc[0, 'Zip Code'], '90210')
         self.assertEqual(merged_data.loc[0, 'Other Data'], 'data1')
         self.assertEqual(merged_data.loc[0, 'latitude'], 34.103)
